0b4eab35975eefdd5864a84978f3833fe91f4120
Fernando Igor Lisboa <fernandoigorlisboa@gmail.com>
2020-06-19 16:42:40 -0300

    Implemented repository CRUD on front-end

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import api from './services/api'
 
-import "./styles.css";
+import "./styles.css"
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => setRepositories(response.data))
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const { data: repo } = await api.post('repositories', {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    })
+    
+    setRepositories([...repositories, repo])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(repo => repo.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.length > 0 && repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
